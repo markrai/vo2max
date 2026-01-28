ab47d47a5798e2b74b1039709c1315dcedff0aca
Mark Rai <markraidc@gmail.com>
2026-01-28 14:30:53 -0500

    patch bump

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 // Centralized version number for the app
 // Update this value when releasing a new version
-const APP_VERSION = '0.9.6';
+const APP_VERSION = '0.9.7';
 
 // Make version globally accessible
 if (typeof window !== 'undefined') {
